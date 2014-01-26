f03de3997e33f48c66208e2302fbc71ef65cdd62
Kay Strobach <github@kay-strobach.de>
2014-01-26 17:52:08 +0100

    vh init

--- Classes/TYPO3/Fluid/ViewHelpers/Security/IfAccesOnControllersActionViewHelper.php
+++ Classes/TYPO3/Fluid/ViewHelpers/Security/IfAccesOnControllersActionViewHelper.php
@@ -32,14 +32,33 @@ class IfAccesOnControllersActionViewHelper extends \TYPO3\Fluid\Core\ViewHelper\
 	protected $accessDecisionManager;
 
 	/**
+	 * @Flow\Inject
+	 * @var \TYPO3\Flow\Package\PackageManagerInterface
+	 */
+	protected $packageManager;
+
+	/**
 	 * renders <f:then> child if access to the given resource is allowed, otherwise renders <f:else> child.
 	 *
-	 * @param string $resource Policy resource
+	 * @param null $package
+	 * @param null $subpackage
+	 * @param null $controller
+	 * @param $action
+	 * @internal param string $resource Policy resource
 	 * @return string the rendered string
 	 * @api
 	 */
-	public function render($resource) {
-		if ($this->hasAccessToResource($resource)) {
+	public function render($package = NULL, $subpackage = NULL, $controller = NULL, $action) {
+		if($package === NULL) {
+			$package = $this->controllerContext->getUriBuilder()->getRequest()->getControllerPackageKey();
+		}
+		if(($package === NULL) && ($subpackage === NULL)) {
+			$subpackage = $this->controllerContext->getUriBuilder()->getRequest()->getControllerSubpackageKey();
+		}
+		if($controller === NULL) {
+			$controller = $this->controllerContext->getUriBuilder()->getRequest()->getControllerName();
+		}
+		if ($this->hasAccessToResource($package, $subpackage, $controller, $action)) {
 			return $this->renderThenChild();
 		} else {
 			return $this->renderElseChild();
@@ -49,16 +68,27 @@ class IfAccesOnControllersActionViewHelper extends \TYPO3\Fluid\Core\ViewHelper\
 	/**
 	 * Check if we currently have access to the given resource
 	 *
-	 * @param string $resource The resource to check
+	 * @param $package
+	 * @param $subpackage
+	 * @param $controller
+	 * @param $action
 	 * @return boolean TRUE if we currently have access to the given resource
 	 */
-	protected function hasAccessToResource($resource) {
+	protected function hasAccessToResource($package, $subpackage, $controller, $action) {
+		$namespace = $this->packageManager->getPackage($package)->getNamespace();
+		$className = $namespace . '\\Controller\\' . $controller . 'Controller';
 		try {
-			$this->accessDecisionManager->decideOnResource($resource);
-		} catch (\TYPO3\Flow\Security\Exception\AccessDeniedException $e) {
+			$this->accessDecisionVoterManager->decideOnJoinPoint(
+				new JoinPoint(
+					$this->objectManager->get($className),
+					$className,
+					$action,
+					array()
+				)
+			);
+		} catch(\TYPO3\Flow\Security\Exception\AccessDeniedException $e) {
 			return FALSE;
 		}
-
 		return TRUE;
 	}
 }
